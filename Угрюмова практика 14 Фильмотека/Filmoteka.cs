42a6d581b932d07557a1f431eee792fbedaa0017
AlexandraUgryumova <124123433+AlexandraUgryumova@users.noreply.github.com>
2023-03-28 15:50:09 +0500

    initial commit

---
+++ Угрюмова практика 14 Фильмотека/Filmoteka.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Угрюмова_практика_14_Фильмотека
+{
+    public abstract class Filmoteka
+    {
+        string name;
+        string production;
+        TimeSpan time;
+        string genre;
+        
+        public Filmoteka(string name, string production, TimeSpan time, string genre)
+        {
+            this.name = name;
+            this.production = production;
+            this.time = time;
+            this.genre = genre;
+        }
+        public string Name { get { return name; } set { name = value; } }
+        public string Production { get; set; }
+        public string Time { get; set; }
+        public string Genre { get; set; }
+
+        public abstract string info();
+    }
+    public  class TVshow : Filmoteka
+    {
+        int count;
+        public TVshow(string name, string production, TimeSpan time, string genre, int count) : base (name, production, time,  genre)
+        {
+            this.count = count;
+        }
+        public override string info()
+        {
+            return $"название:  {Name}, производство: {Production}, время: {Time}, жанр: {Genre}, количество серий:{count}";
+        }
+
+    }
+    class FeatureFilm : Filmoteka
+    {
+        DateTime yearOfIssue;
+        bool color;
+        public FeatureFilm(string name, string production, TimeSpan time, string genre, DateTime yearOfIssue, bool color) : base (name, production, time, genre)
+        {
+
+        }
+        public override string info()
+        {
+            return "";
+        }
+    }
+}
---
+++ Угрюмова практика 14 Фильмотека/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Угрюмова_практика_14_Фильмотека
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            FeatureFilm fm = new FeatureFilm("Секс в большом городе", "HBO", new TimeSpan(1, 20, 10), "комедия", new DateTime(12, 12, 12), false);
+        }
+    }
+}
